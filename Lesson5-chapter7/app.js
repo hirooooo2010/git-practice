b256d8f6cbdb4fb6e72428adb417f12956a0ddfc
HirokiNagatomo <45706277+hirooooo2010@users.noreply.github.com>
2023-09-13 22:12:18 +0900

    提出課題_JavaScript_2回目

--- Lesson5-chapter7/app.js
+++ Lesson5-chapter7/app.js
@@ -1,11 +1,13 @@
-
 for (let i = 1; i <= 50; i++) {
   if (i === 50) {
     alert("50回カウントが終わりました。");
-  } else if (i % 10 === 0) {
-    console.log("今" + i  + "回ループしました。");
   } else if (i % 4 === 0) {
     console.log("4で割れる数です。" + i);
+    if (i % 10 === 0) {
+      console.log("今" + i  + "回ループしました。");
+    }
+  } else if (i % 10 === 0) {
+    console.log("今" + i  + "回ループしました。");
   } else {
     console.log(i);
   }
